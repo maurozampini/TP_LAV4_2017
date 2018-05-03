28165cf848921c81c1bbc258364fb02ad0d05744
maurozampini <mauro.zampini@hotmail.com>
2018-05-03 20:11:52 -0300

    probando si buildea

--- src/app/tateti/tateti.component.ts
+++ src/app/tateti/tateti.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import '../../assets/tateti.js';
+/*import hola from  '../../assets/tateti.js';*/
+declare var jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-tateti',
@@ -7,6 +9,19 @@ import '../../assets/tateti.js';
   styleUrls: ['./tateti.component.css']
 })
 export class TatetiComponent implements OnInit {
-  ngOnInit(){}
+  
+  Element: HTMLElement;  
+  
+  ngOnInit(){
+
+  }
+ 
+  x () {
+
+
+console.log();
+  }
+
+
 
 }
